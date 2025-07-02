fd7672bc57670a829ae07bc9b5978bb0b49e298a
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-07-02 15:01:54 +0530

    small change in mongodb connection

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const Signup = mongoose.model('Signup', signupSchema);
 
 // MongoDB connection
 // MongoDB connection
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGO_URI;
 
 async function connect() {
     try {
